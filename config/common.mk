PixelExperience/vendor_aosp

vendor: Allow disabling CarrierSettings
Change-Id: Ic614d5995c33f35dad89c812706178fcacc43e87

config/common.mk
@@ -167,4 +167,7 @@ include vendor/aosp/config/audio.mk
 # Fonts
 include vendor/aosp/config/fonts.mk
 
+# CarrierSettings
+include vendor/aosp/CarrierSettings/config.mk
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk

ahmedmoselhi/vendor_xtended

config/common.mk
@@ -212,6 +212,9 @@ include vendor/xtended/config/audio.mk
 # Fonts
 include vendor/xtended/config/fonts.mk
 
+# CarrierSettings
+include vendor/xtended/CarrierSettings/config.mk
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/xtended/config/partner_gms.mk
 -include packages/apps/Plugins/plugins.mk